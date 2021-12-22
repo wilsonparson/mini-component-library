3d9d941821cc582efb4bbe67fa4eb29a2998bc1f
Wilson Parson <wilson.parson@gmail.com>
2021-12-22 00:37:22 -0700

    Finish IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,81 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+    "--font-size": "14px",
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+    "--font-size": "18px",
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const styles = STYLES[size];
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} strokeWidth={2} />
+      </IconWrapper>
+      <Input
+        placeholder={placeholder}
+        style={{
+          "--width": width + "px",
+          "--height": styles.height + "px",
+          "--border-thickness": styles.borderThickness + "px",
+          "--font-size": styles.fontSize + "px",
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
+
+const Input = styled.input`
+  height: var(--height);
+  border: none;
+  color: inherit;
+  border-bottom: solid var(--border-thickness) ${COLORS.black};
+  font-size: var(--font-size);
+  font-weight: 700;
+  padding-left: var(--height);
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+  outline-offset: 2px;
+  width: var(--width);
+`;
 
 export default IconInput;
